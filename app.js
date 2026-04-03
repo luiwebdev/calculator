723716317c1093c885815b89de2a04c0be01e4c0
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-04-03 12:43:25 -0700

    Add operate function

--- app.js
+++ app.js
@@ -22,3 +22,17 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, number1, number2) {
+
+    switch (operator) {
+        case "+":
+            return add(number1, number2);
+        case "-":
+            return substract(number1, number2);
+        case "*":
+            return multiply(number1, number2);
+        case "/":
+            return divide(number1, number2);
+    }
+}
