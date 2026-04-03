834a1d81993b783314183eb764021422dde7bf23
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-04-03 12:42:43 -0700

    Add variables and basic math functions

---
+++ app.js
@@ -0,0 +1,24 @@
+let number1 = "";
+let number2 = "";
+let operator = "";
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button")
+
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
