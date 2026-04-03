8757bd3a935ecb32168a181656e9c93e2ee16c76
luiwebdev <148648445+luiwebdev@users.noreply.github.com>
2026-04-03 12:44:19 -0700

    Add functions to get values

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ function divide(a, b) {
     return a / b;
 }
 
+
 function operate(operator, number1, number2) {
 
     switch (operator) {
@@ -34,5 +35,34 @@ function operate(operator, number1, number2) {
             return multiply(number1, number2);
         case "/":
             return divide(number1, number2);
+    }
+}
+
+
+function getOperator(item) {
+    if (item.target.className == "operator") {
+        operator = item.target.textContent;
+    }
+}
+
+function getNumber1(item) {
+    if (operator == "" && item.target.className == "number") {
+        number1 = number1 + item.target.textContent;
+    }
+}
+
+function getNumber2(item) {
+    if (operator !== "" && item.target.className == "number") {
+        number2 = number2 + item.target.textContent;
+    }
+}
+
+function getDisplay() {
+    display.textContent = number1 + operator + number2;
+}
+
+function getResult(item) {
+    if (item.target.className == "equal") {
+        display.textContent = operate(operator, Number(number1), Number(number2));
     }
 }
